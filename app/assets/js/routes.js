03a3b3404ee46666556bc5dd15099b6f4c768318
Dommert <DommertEnterprises@Gmail.com>
2016-03-24 18:52:20 -0700

    routes
    new routes

--- app/assets/js/routes.js
+++ app/assets/js/routes.js
@@ -7,6 +7,26 @@ webApp.config(['$urlRouterProvider', '$stateProvider',
             templateUrl: 'templates/partials/frontpage.html',
             controller: 'mainCtrl'
           })
+          .state('pizzas', {
+            url: '/',
+            templateUrl: 'templates/partials/pizzas.html',
+            controller: 'mainCtrl'
+          })
+          .state('toppings', {
+            url: '/',
+            templateUrl: 'templates/partials/toppings.html',
+            controller: 'mainCtrl'
+          })
+          .state('pizza', {
+            url: '/pizza/:id',
+            templateUrl: 'templates/partials/pizza.html',
+            controller: 'mainCtrl'
+          })
+          .state('newPizza', {
+            url: '/',
+            templateUrl: 'templates/partials/newPizza.html',
+            controller: 'mainCtrl'
+          })                                        
           .state('about', {
             url: '/about',
             templateUrl: 'templates/partials/about.html',
