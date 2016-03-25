f0acaf145db7849847d59652904925278ac4d038
Dommert <DommertEnterprises@Gmail.com>
2016-03-25 00:40:17 -0700

    pizzas and toppings

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -8,6 +8,18 @@ webApp.controller('mainCtrl',
 webApp.controller('buttonCtrl', 
 	function($scope){
 		$scope.singleModel = 1;
+    });
+
+webApp.controller('toppingsCtrl', function($scope, $http) {
+    $http.get("https://pizzaserver.herokuapp.com/toppings")
+    .then(function(response) {
+        $scope.toppings = response.data;
+    })
 });
 
 
+webApp.controller('pizzasCtrl', function($scope, $http){
+    $http.get("https://pizzaserver.herokuapp.com/pizzas")
+    .then(function(response){
+        $scope.pizzas = response.data;})
+});
--- app/assets/js/routes.js
+++ app/assets/js/routes.js
@@ -7,30 +7,34 @@ webApp.config(['$urlRouterProvider', '$stateProvider',
             templateUrl: 'templates/partials/frontpage.html',
             controller: 'mainCtrl'
           })
+
           .state('pizzas', {
-            url: '/',
+            url: '/pizzas',
             templateUrl: 'templates/partials/pizzas.html',
-            controller: 'mainCtrl'
+            controller: 'pizzasCtrl'
           })
-          .state('toppings', {
-            url: '/',
-            templateUrl: 'templates/partials/toppings.html',
-            controller: 'mainCtrl'
-          })
+
           .state('pizza', {
             url: '/pizza/:id',
             templateUrl: 'templates/partials/pizza.html',
             controller: 'mainCtrl'
           })
+
           .state('newPizza', {
             url: '/newPizza',
             templateUrl: 'templates/partials/newPizza.html',
             controller: 'mainCtrl'
-          })                                        
+          })
+
+          .state('toppings', {
+            url: '/toppings',
+            templateUrl: 'templates/partials/toppings.html',
+            controller: 'toppingsCtrl'
+          }) 
+
           .state('about', {
             url: '/about',
             templateUrl: 'templates/partials/about.html',
             controller: 'mainCtrl'
           });
-  }
-]);
+  }]);
