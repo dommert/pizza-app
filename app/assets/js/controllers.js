fe22b1eb88b2b18e90b00dea67d19bbe72fbe336
Dommert <DommertEnterprises@Gmail.com>
2016-03-26 03:28:14 -0700

    controller updates

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -1,4 +1,4 @@
-
+// ------ toppings Controller  ---------> 
 webApp.controller('toppingsCtrl', function($scope, $http) {
     $http.get("https://pizzaserver.herokuapp.com/toppings")
     .then(function(response) {
@@ -6,9 +6,51 @@ webApp.controller('toppingsCtrl', function($scope, $http) {
     })
 });
 
-
-webApp.controller('pizzasCtrl', function($scope, $http){
-    $http.get("https://pizzaserver.herokuapp.com/pizzas")
-    .then(function(response){
-        $scope.pizzas = response.data;})
+// ------ newTopping Controller  ---------> 
+webApp.controller('newToppingCtrl', function($scope, $http, $location){
+	$scope.formData = {};
+	$scope.toppingForm = function() {
+		$http({
+			method : 'POST',
+			url : 'https://pizzaserver.herokuapp.com/toppings',
+			dataType : 'json',
+			data : {name: $scope.name}
+		})
+		.success(function(data){
+			if (data.errors) {
+				$scope.errorName = data.errors.name;
+			}
+			$location.path('/');
+		})
+	};
 });
+
+// ------ Pizzas Controller  ---------> 
+webApp.controller('pizzasCtrl', function($scope, $http){
+    $http.get("https://pizzaserver.herokuapp.com/pizzas", { cache: true})
+    .then(function(response){
+        $scope.pizzas = response.data;
+    });
+ });
+
+// ------ Pizza Controller  ---------> 
+webApp.controller('pizzaCtrl', function($stateParams, $scope, $http){
+	$scope.pizzaId = $stateParams.pizzaId;
+	$scope.pizzaName = $stateParams.pizzaName;
+	$scope.pizzaDesc = $stateParams.pizzaDesc;
+	
+	$http.get("https://pizzaserver.herokuapp.com/pizzas/"+ $stateParams.pizzaId + "/toppings")
+    .then(function(response){
+        $scope.toppings = response.data;
+    });
+
+   $http.get("https://pizzaserver.herokuapp.com/pizzas")
+    .then(function(response){
+        $scope.pizzas = response.data;
+    });
+   $scope.orderPizza = function(text) {
+  alert(text);
+};
+
+});
+
